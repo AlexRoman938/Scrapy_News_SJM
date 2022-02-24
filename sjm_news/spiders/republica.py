b9a2db5ff88d81caf5668a66308ceeaa293b73ad
Alexander <aroman.link11@gmail.com>
2022-02-23 23:14:43 -0500

    Se logro dar click en selenium para el ver más, pero no scrapeo lo que quiero

--- sjm_news/spiders/republica.py
+++ sjm_news/spiders/republica.py
@@ -3,6 +3,8 @@ from urllib.request import Request
 import scrapy
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+import os
 
 """
 What do I want? :
@@ -11,7 +13,7 @@ DIARIO LA REPÚBLICA
 
 
 
-Href del artículo : response.xpath('//li[@class = "PostSectionListLI"]/h2[@class = "PostSectionListH3"]/a[@class = "PostSectionListA"]/@href').getall()
+Href del artículo : response.css('.PostSectionListA ::attr(href)').getall()
 
 Luego entrar al artículo
 
@@ -24,6 +26,9 @@ fecha : response.xpath("//div[@class = 'comp-autor-dateTime']/time/text()").get(
 Posiblemente hacer el ver más de abajo
 
 """
+
+gecko = os.path.normpath(os.path.join(os.path.dirname(__file__), 'geckodriver'))
+binary = FirefoxBinary('C:\\Program Files\\Mozilla Firefox\\firefox.exe')
 
 class SJMSpider(scrapy.Spider):
 
@@ -43,7 +48,7 @@ class SJMSpider(scrapy.Spider):
             'sjm_news_republica.json': {
                 'format': 'json',
                 'encoding': 'utf8',
-                'fields': ['link_republica', 'title_republica', 'subtitle_republica', 'fecha'],
+                'fields': ['link_republica',],
                 'overwrite': True
             }
         }
@@ -51,45 +56,54 @@ class SJMSpider(scrapy.Spider):
 
     def __init__(self):
         
-        self.driver = webdriver.Chrome(executable_path = 'C:\\Users\\sasuk\\Downloads\\web_driver\\chromedriver.exe')
+        self.driver = webdriver.Firefox(firefox_binary= binary,
+                         executable_path= 'C:\\Users\\sasuk\gecko\\geckodriver.exe')
 
         
-
-
-
     def parse(self, response):
         
         self.driver.get('https://larepublica.pe/tag/san-juan-de-miraflores/')
 
         while True:
 
+            button_down = self.driver.find_element(By.CSS_SELECTOR, 'button#btnLoadMore')
+
             try:
 
-                button_down = self.driver.find_element(By.CSS_SELECTOR, 'button#btnLoadMore')
-
-                url = "https://larepublica.pe/tag/san-juan-de-miraflores/"
-
-                yield Request(url, callback = self.parse2)
 
                 button_down.click()
+                break
+                
+ 
 
             except:
 
                 break
+        
+        href_republica = response.css('.PostSectionListA ::attr(href)').getall()
+
+        yield{
+
+             'link_republica' : href_republica
+
+        }
+
+                
+        
 
         
-    def parse2(self, response):
+    """def parse2(self, response):
 
         href_republica = response.xpath('//li[@class = "PostSectionListLI"]/h2[@class = "PostSectionListH3"]/a[@class = "PostSectionListA"]/@href').getall()
 
         for href in href_republica:
 
             yield response.follow(href, callback = self.parse_link2,
-                                                cb_kwargs = {'url_republica': response.urljoin(href)})
+                                                cb_kwargs = {'url_republica': response.urljoin(href)})"""
     
 
 
-    def parse_link2(self, response, **kwargs):
+    """def parse_link2(self, response, **kwargs):
 
         link_republica = kwargs['url_republica']
         title_republica = response.xpath("//h1/text()").get()
@@ -103,22 +117,6 @@ class SJMSpider(scrapy.Spider):
             'subtitle_republica' : subtitle_republica,
             'fecha' : fecha
 
-        }
-
-    """def parse_link(self, response, **kwargs):
-        
-        link_comercio = kwargs['url_comercio']
-        title_comercio = response.xpath('//h1/text()').get()
-        subtitle_comercio = response.xpath('//h2/text()').get()
-        fecha = response.css('.story-contents__time ::text').get()
-
-        yield{
-
-            'link_comercio' : link_comercio,
-            'title_comercio' : title_comercio,
-            'subtitle_comercio' : subtitle_comercio,
-            'fecha' : fecha
-        }
-"""
+        }"""
 
     
